1ada8c628dc29f0d9bb8712be4041d20724f2e78
vanquiche <stevevang2010@hotmail.com>
2023-03-14 19:53:26 +0900

    fix: remove next Image completely

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -2,25 +2,25 @@ import { getAllPostIds, getPostData } from '@/lib/posts';
 import { PostData } from '@/types';
 import { parseISO, format } from 'date-fns';
 import { MDXRemote } from 'next-mdx-remote';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 const Heading = (props: any) => {
   return <h2 style={{ color: 'tomato' }}>{props.children}</h2>;
 };
 
-const components = {
-  h2: (props: any) => <Heading {...props} />,
-  img: (props: any) => (
-    <Image
-      style={{ objectFit: 'contain' }}
-      src={props.src}
-      alt={props.alt}
-      width={700}
-      height={700}
-      loading='lazy'
-    />
-  ),
-};
+// const components = {
+//   h2: (props: any) => <Heading {...props} />,
+//   img: (props: any) => (
+//     <Image
+//       style={{ objectFit: 'contain' }}
+//       src={props.src}
+//       alt={props.alt}
+//       width={700}
+//       height={700}
+//       loading='lazy'
+//     />
+//   ),
+// };
 
 export default function Blog({ source }: { source: PostData }) {
   const date = parseISO(source.meta.date);
