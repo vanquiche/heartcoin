91762b1f51c517b60ada81c5c671c00612f42688
vanquiche <stevevang2010@hotmail.com>
2023-03-14 21:30:48 +0900

    feat: add cloudinary image component

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -3,24 +3,16 @@ import { PostData } from '@/types';
 import { parseISO, format } from 'date-fns';
 import { MDXRemote } from 'next-mdx-remote';
 // import Image from 'next/image';
+import { CldImage } from 'next-cloudinary';
 
 const Heading = (props: any) => {
   return <h2 style={{ color: 'tomato' }}>{props.children}</h2>;
 };
 
-// const components = {
-//   h2: (props: any) => <Heading {...props} />,
-//   img: (props: any) => (
-//     <Image
-//       style={{ objectFit: 'contain' }}
-//       src={props.src}
-//       alt={props.alt}
-//       width={700}
-//       height={700}
-//       loading='lazy'
-//     />
-//   ),
-// };
+const components = {
+  h2: (props: any) => <Heading {...props} />,
+  img: (props: any) => <CldImage src={props.src} alt={props.alt} />,
+};
 
 export default function Blog({ source }: { source: PostData }) {
   const date = parseISO(source.meta.date);
